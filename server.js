49ff8ad35bf697a85a41b1030b769aeac2be99af
Jared Dye <jjtd2000@gmail.com>
2023-08-07 20:13:07 -0700

    cors change

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
@@ -13,6 +14,7 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to MongoDB"));
 
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
 	res.setHeader(
 		"Access-Control-Allow-Origin",
@@ -30,7 +32,6 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Private-Network", true);
 	//  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
 	res.setHeader("Access-Control-Max-Age", 7200);
-
 	next();
 });
 
